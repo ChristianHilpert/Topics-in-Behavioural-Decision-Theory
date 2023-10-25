2dda562ef7004f2f693d262cdf4ff75cb2314495
christianhilpert <christian.hilpert@posteo.de>
2023-10-25 15:14:20 +0800

    Polished chapters 1 and 2.

--- src/sandbox/Main.m
+++ src/sandbox/Main.m
@@ -3,13 +3,14 @@ x = 28;
 y = 2;
 p = 0.2;
 g = 0.1;
-l = 2.25;
+l = 2.5;
 r = 13;
 
 xx = (w-3):0.01:(w+x+10);
 
-width = 8;
+width = 10;
 height = 4;
+
 figure1 = figure('visible', 'off');
 set(gcf, 'Position', [0 0 width*100, height*100]);
 axes1 = axes('Parent', figure1, 'FontSize', 15);
@@ -33,9 +34,10 @@ left = (papersize(1)- width)/2;
 bottom = (papersize(2)- height)/2;
 myfiguresize = [left, bottom, width, height];
 set(gcf,'PaperPosition', myfiguresize);
+saveas(figure1, 'expected_utility.png');
 
 g = 0.5;
-xx = 2:0.01:25;
+xx = (2:0.01:25) - r;
 
 figure1 = figure('visible', 'off');
 set(gcf, 'Position', [0 0 width*100, height*100]);
@@ -43,15 +45,15 @@ axes1 = axes('Parent', figure1, 'FontSize', 15);
 box(axes1, 'on')
 hold(axes1, 'on')
 
-plot(xx(xx>=r), (xx(xx>=r)-r).^g, 'LineWidth', 3, 'Color', [0.2, 0.2 0.7])
-plot(xx(xx<=r), -l*(-(xx(xx<=r)-r)).^g, 'LineWidth', 3, 'Color', [0.2, 0.2 0.7])
+plot(xx(xx >= 0), (xx(xx >= 0)).^g, 'LineWidth', 3, 'Color', [0.2, 0.2 0.7])
+plot(xx(xx <= 0), -l*(-(xx(xx <= 0))).^g, 'LineWidth', 3, 'Color', [0.2, 0.2 0.7])
 plot(xx, xx * 0, 'LineWidth', 3)
 
 hold off
-xlabel("Wealth", "FontSize", 15)
+xlim([-10, 10])
+xlabel("Gain/Loss", "FontSize", 15)
 ylabel("Utility", "FontSize", 15)
 
-xlim([5,25])
 % File formatting.
 set(gcf,'InvertHardcopy','on');
 set(gcf,'PaperUnits', 'inches');
@@ -65,9 +67,9 @@ saveas(figure1, 'cpt_utility.png');
 
 
 p_gain = 0:0.01:1;
-dp =0.64;
+dp =0.65;
 
-figure1 = figure('visible', 'on');
+figure1 = figure('visible', 'off');
 set(gcf, 'Position', [0 0 width*100, height*100]);
 axes1 = axes('Parent', figure1, 'FontSize', 15);
 box(axes1, 'on')
